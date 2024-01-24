ba1744506760f03e1c9a893f4f2d11bcdf9e7167
Andy Cheung <andy@vantage.sh>
2024-01-24 13:21:08 -0500

    Generate clients.

--- vantagev1/generate.sh
+++ vantagev1/generate.sh
@@ -2,9 +2,8 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
-echo ${pwd}
-# curl https://api.vantage.sh/v1/swagger.json | jq . > swagger.json
-# swagger generate client -f swagger.json -c vantage vantagev1
+curl https://api.vantage.sh/v1/swagger.json | jq . > swagger.json
+swagger generate client -f swagger.json -c vantage vantagev1
 # go mod tidy
 popd
 
