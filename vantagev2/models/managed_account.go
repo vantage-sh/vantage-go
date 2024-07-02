8fa1562100616e6d00b47c01109f8d705e85b35c
Andy Cheung <andy@vantage.sh>
2024-07-01 21:42:03 -0400

    Generate clients.

--- vantagev2/models/managed_account.go
+++ vantagev2/models/managed_account.go
@@ -21,7 +21,7 @@ type ManagedAccount struct {
 	AccessCredentialTokens []string `json:"access_credential_tokens"`
 
 	// The tokens for the Billing Rules assigned to the Managed Account.
-	BillingRuleTokens string `json:"billing_rule_tokens,omitempty"`
+	BillingRuleTokens []string `json:"billing_rule_tokens"`
 
 	// contact email
 	ContactEmail string `json:"contact_email,omitempty"`
